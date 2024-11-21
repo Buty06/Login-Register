b23a271ef7a5eafecdd8c7b0e915af75e09d7dbc
Pelicie <mecagoenlashormigas@gmail.com>
2024-11-21 01:41:36 -0500

    Terminando la funcion de js, no estoy orgulloso, pero al menos cumple su papel

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const $loginButton = document.getElementById("change_login");
+const $signupButton = document.getElementById("change_signup");
+const $signupCard = document.getElementById("card1");
+const $loginCard = document.getElementById("card2");
 
+$loginButton.addEventListener("click", () => {
+  $signupCard.classList.toggle("display");
+  $loginCard.classList.toggle("display");
+
+  setTimeout(() => {
+    $signupCard.classList.toggle("display2");
+    $loginCard.classList.toggle("display2");
+    $signupCard.classList.toggle("display3");
+    $loginCard.classList.toggle("display3");
+  }, 500);
+});
+
+$signupButton.addEventListener("click", () => {
+  $signupCard.classList.toggle("display");
+  $loginCard.classList.toggle("display");
+
+  setTimeout(() => {
+    $signupCard.classList.toggle("display2");
+    $loginCard.classList.toggle("display2");
+    $signupCard.classList.toggle("display3");
+    $loginCard.classList.toggle("display3");
+  }, 500);
+});
